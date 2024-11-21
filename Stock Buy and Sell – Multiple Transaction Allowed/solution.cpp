dcb314b762108346aaada89bc03639ca1b078a30
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2024-11-21 21:51:34 +0530

    Add files via upload

---
+++ Stock Buy and Sell – Multiple Transaction Allowed/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        int n=prices.size();
+        int ans=0;
+        int buy=prices[0];
+        for(int i=0;i<n;i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+            }
+            else{
+                ans+=prices[i]-buy;
+                while(i<n-1){
+                    if(prices[i]<prices[i+1]){
+                        buy=prices[i];
+                        break;
+                    }
+                    else
+                    i++;
+                }
+            }
+        }
+        return ans;
+    }
+};

1a89f2753998e42b69d7dc8c06e7a7ce2591fc2e
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2025-01-03 06:28:18 +0530

    Add files via upload

---
+++ Count Subarrays with given XOR/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long subarrayXor(int arr[], int k) {
+        // code here
+       int n = arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int xor=0;
+        int ans=0;
+        for(int i=0; i<n; i++){
+            xor^=arr[i];
+            
+            if(xor==k)
+            ans++;
+            
+            if(map.containsKey(xor^k)){
+                ans += map.get(xor^k);
+            }
+            
+            map.put(xor, map.getOrDefault(xor,0)+1);
+        }
+        return ans;
+    }
+}

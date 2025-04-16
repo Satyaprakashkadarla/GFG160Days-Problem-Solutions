1129e13bcc412d1f10873776403344e60fdd650b
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2025-04-16 20:43:29 +0530

    Add files via upload

---
+++ Graph/Floyd Warshall/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void floydWarshall(int[][] dist) {
+        int V = dist.length;
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (dist[i][k] != 100000000 && dist[k][j] != 100000000 &&
+                        dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+    }
+}

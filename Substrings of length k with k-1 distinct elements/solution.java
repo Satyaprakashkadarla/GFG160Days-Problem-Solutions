a16cc3407c353f87252a3a621c9dd509f148b558
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2025-07-01 22:34:20 +0530

    Add files via upload

---
+++ Substrings of length k with k-1 distinct elements/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int substrCount(String s, int k) {
+        int n      = s.length();
+        int result = 0;
+
+        for (int i = 0; i <= n - k; i++) {
+            int[] freq = new int[26]; // reset frequency array for each substring
+
+            for (int j = i; j < i + k; j++) {
+                freq[s.charAt(j) - 'a']++; // count frequency of characters in current substring
+            }
+
+            int count = 0;
+            for (int l = 0; l < 26; l++) {
+                if (freq[l] > 0) {
+                    count++; // count distinct characters
+                }
+            }
+
+            if (count == k - 1) {
+                result++; // valid substring
+            }
+        }
+
+        return result;
+    }
+}

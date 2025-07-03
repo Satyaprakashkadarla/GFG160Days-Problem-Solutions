5ebd7cfc1e48906020e4be258e760e7b14ba36e7
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2025-07-03 21:39:08 +0530

    Add files via upload

---
+++ Longest Substring with K Uniques/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestKSubstr(String s, int k) {
+        int n = s.length();
+        int i = 0, j = 0;
+        int cnt = 0;
+        int maxi = -1;
+        int[] fre = new int[26];
+        while (j < n) {
+
+            fre[s.charAt(j) - 'a']++;
+            if (fre[s.charAt(j) - 'a'] == 1) cnt++;
+            while (cnt > k) {
+                fre[s.charAt(i) - 'a']--;
+                if (fre[s.charAt(i) - 'a'] == 0) cnt--;
+                i++;
+            }
+            if (cnt == k) {
+                maxi = Math.max(maxi, j - i + 1);
+            }
+
+            j++;
+        }
+        return maxi;
+    }
+}

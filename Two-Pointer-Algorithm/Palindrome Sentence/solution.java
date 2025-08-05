830585246349da1b85b68c5c2c26253b9324d6f4
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2025-08-05 14:31:11 +0530

    Add files via upload

---
+++ Two-Pointer-Algorithm/Palindrome Sentence/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalinSent(String s) {
+        StringBuilder cleaned = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+        String cleanedString = cleaned.toString();
+        int left = 0;
+        int right = cleanedString.length() - 1;
+        while (left < right) {
+            if (cleanedString.charAt(left) != cleanedString.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}

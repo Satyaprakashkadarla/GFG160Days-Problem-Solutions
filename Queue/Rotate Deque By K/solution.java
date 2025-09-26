08c03e9db4159cbc6c1bb263fc86f977222a6030
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2025-09-26 21:09:10 +0530

    Add files via upload

---
+++ Queue/Rotate Deque By K/solution.java
@@ -0,0 +1,22 @@
+import java.util.Deque;
+
+class Solution {
+    public static void rotateDeque(Deque<Integer> dq, int type, int k) {
+        int n = dq.size();
+        if (n == 0) return;
+        
+        k = k % n; 
+
+        if (type == 1) {
+            for (int i = 0; i < k; i++) {
+                int last = dq.pollLast();
+                dq.addFirst(last);
+            }
+        } else if (type == 2) {
+            for (int i = 0; i < k; i++) {
+                int first = dq.pollFirst();
+                dq.addLast(first);
+            }
+        }
+    }
+}

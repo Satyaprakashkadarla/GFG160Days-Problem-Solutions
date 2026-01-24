e2073737d92a88222ad08611a568026f9f2e7fef
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-01-24 05:54:22 +0530

    Add files via upload

---
+++ Arrays/Minimize Maximum Pair Sum in Array/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minPairSum(int[] nums) {
+        int[] count = new int[100001];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i : nums) {
+            count[i]++;
+            max = Math.max(max, i);
+            min = Math.min(min, i);
+        }
+        int low = min, high = max;
+        max = Integer.MIN_VALUE;
+        while(low <= high) {
+            if(count[low] == 0) low++; 
+            else if(count[high] == 0) high--;
+            else {
+                max = Math.max(low + high, max);
+                count[low]--;
+                count[high]--;
+            }
+        }
+        return max;
+    }
+}

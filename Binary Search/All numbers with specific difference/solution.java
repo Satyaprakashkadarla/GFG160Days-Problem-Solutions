0fcab1343b8934e40a6b2852341cd1819e8853b7
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-02-13 21:32:57 +0530

    Add files via upload

---
+++ Binary Search/All numbers with specific difference/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int getCount(int n, int d) {
+        int low = 1, high = n;
+        int ans = -1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid - sumOfDigits(mid) >= d) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        if (ans == -1) return 0;
+        return n - ans + 1;
+    }
+    
+    private int sumOfDigits(int x) {
+        int sum = 0;
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        return sum;
+    }
+}

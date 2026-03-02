910d31986ba4fdb0cdc0b3aa4fd36365ca02437c
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-03-02 15:40:39 +0530

    Add files via upload

---
+++ Arrays/Trapping Rain Water/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxWater(int arr[]) {
+        int n = arr.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        
+        leftMax[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], arr[i]);
+        }
+        
+        rightMax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], arr[i]);
+        }
+        
+        int water = 0;
+        for (int i = 0; i < n; i++) {
+            water += Math.min(leftMax[i], rightMax[i]) - arr[i];
+        }
+        return water;
+    }
+}

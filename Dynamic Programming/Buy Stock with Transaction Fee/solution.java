e6a277c57a4e6a14ec2cd7b213d7511c8eb8869c
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-03-31 05:56:11 +0530

    Add files via upload

---
+++ Dynamic Programming/Buy Stock with Transaction Fee/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int arr[], int k) {
+        int n = arr.length;
+        if (n == 0) return 0;
+        
+        int cash = 0;
+        int hold = -arr[0];
+        
+        for (int i = 1; i < n; i++) {
+            cash = Math.max(cash, hold + arr[i] - k);
+            hold = Math.max(hold, cash - arr[i]);
+        }
+        
+        return cash;
+    }
+}

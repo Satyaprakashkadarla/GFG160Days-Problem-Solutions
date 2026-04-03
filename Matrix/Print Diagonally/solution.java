5c1ac6542196de5f7406be38d16c5646bc542d9b
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-04-03 15:18:58 +0530

    Add files via upload

---
+++ Matrix/Print Diagonally/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    static ArrayList<Integer> diagView(int mat[][]) {
+        int n = mat.length;
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for (int sum = 0; sum <= 2 * (n - 1); sum++) {
+            int row = Math.max(0, sum - (n - 1));
+            int col = Math.min(n - 1, sum);
+            while (row < n && col >= 0) {
+                result.add(mat[row][col]);
+                row++;
+                col--;
+            }
+        }
+        return result;
+    }
+}

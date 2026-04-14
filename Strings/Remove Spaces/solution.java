4595e195551fede0d7917cc7e10ab1ab1ead538e
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-04-14 07:07:07 +0530

    Add files via upload

---
+++ Strings/Remove Spaces/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    String removeSpaces(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch != ' ') {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}

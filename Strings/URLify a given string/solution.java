4c2dbdd9246664912f82e8f776ce6a2679088991
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-04-15 06:10:04 +0530

    Add files via upload

---
+++ Strings/URLify a given string/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    String URLify(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}

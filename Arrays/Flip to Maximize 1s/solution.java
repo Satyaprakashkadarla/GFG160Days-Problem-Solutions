19db976193dea741faf8d0bad88f944379776f38
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-04-18 06:02:51 +0530

    Add files via upload

---
+++ Arrays/Flip to Maximize 1s/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    int maxOnes(int[] arr) {
+        int totalOnes = 0;
+        for (int num : arr) {
+            if (num == 1) totalOnes++;
+        }
+        
+        int curr = 0, maxGain = 0;
+        for (int num : arr) {
+            int val = (num == 0) ? 1 : -1;
+            curr = Math.max(val, curr + val);
+            maxGain = Math.max(maxGain, curr);
+        }
+        
+        return totalOnes + maxGain;
+    }
+}

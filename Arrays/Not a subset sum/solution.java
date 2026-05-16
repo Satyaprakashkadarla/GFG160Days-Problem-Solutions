8134ed8a0022964ad5c619b1bd32a3bb9e74b704
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-05-16 18:43:48 +0530

    Add files via upload

---
+++ Arrays/Not a subset sum/solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int findSmallest(int[] arr) {
+        Arrays.sort(arr);
+        int res = 1;
+        for (int num : arr) {
+            if (num > res) break;
+            res += num;
+        }
+        return res;
+    }
+}

9a12e72eae744226e3874a2bfab67ff56d71d3dc
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-05-24 08:32:43 +0530

    Add files via upload

---
+++ Arrays/Last Coin in a Game of Alternates/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int coin(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            if (arr[left] >= arr[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return arr[left];
+    }
+}

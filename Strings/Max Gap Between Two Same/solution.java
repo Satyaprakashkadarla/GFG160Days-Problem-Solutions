81e1b14d2f1dfc08f081c2cd8513321af01b1cab
SatyaPrakash93 <156898856+Satyaprakashkadarla@users.noreply.github.com>
2026-07-05 09:29:03 +0530

    Add files via upload

---
+++ Strings/Max Gap Between Two Same/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int maxCharGap(String s) {
+        int[] first = new int[26];
+        Arrays.fill(first, -1);
+        int maxGap = -1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+            if (first[idx] == -1) {
+                first[idx] = i;
+            } else {
+                maxGap = Math.max(maxGap, i - first[idx] - 1);
+            }
+        }
+        return maxGap;
+    }
+}
